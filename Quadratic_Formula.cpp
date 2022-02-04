900b0a3ebb613c56df6ce44063bec793e802c773
cntis <91499076+cntis@users.noreply.github.com>
2022-02-04 14:51:04 +0100

    Create Quadratic_Formula.cpp

---
+++ Quadratic_Formula.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  
+  double a;
+  double b;
+  double c;
+
+  std::cout<< "Enter a:";
+  std::cin>>a;
+
+  std::cout<< "Enter b:";
+  std::cin>>b;
+
+  std::cout<<"Enter c:";
+  std::cin>>c;
+
+  double root1;
+  double root2;
+
+  root1= (-b+std::sqrt(b*b-4*a*c))/ (2*a);
+  root2= (-b-std::sqrt(b*b-4*a*c))/ (2*a);
+
+  std::cout<<"Root1 is " << root1 <<"\n";
+  std::cout<<"Root2 is " << root2 <<"\n";
+return 0;
+}
